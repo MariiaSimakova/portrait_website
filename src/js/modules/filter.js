05a0c83b241a88226763a85cd9f4b82ea59f8d8d
MariiaSimakova <mariiasimakova6@gmail.com>
2024-11-25 17:01:46 +1000

    add filter

---
+++ src/js/modules/filter.js
@@ -0,0 +1,60 @@
+const filter = () => {
+  const menu = document.querySelector(".portfolio-menu"),
+    items = menu.querySelectorAll("li"),
+    // btnAll = menu.querySelector(".all"),
+    // btnLovers = menu.querySelector(".lovers"),
+    // btnChef = menu.querySelector(".chef"),
+    // btnGirl = menu.querySelector(".girl"),
+    // btnGuy = menu.querySelector(".guy"),
+    // btnGrandmother = menu.querySelector(".grandmother"),
+    // btnGranddad = menu.querySelector(".granddad"),
+    wrapper = document.querySelector(".portfolio-wrapper"),
+    markAll = wrapper.querySelectorAll(".all"),
+    // markGirl = wrapper.querySelectorAll(".girl"),
+    // markLovers = wrapper.querySelectorAll(".lovers"),
+    // markChef = wrapper.querySelectorAll(".chef"),
+    // markGuy = wrapper.querySelectorAll(".guy"),
+    no = document.querySelector(".portfolio-no");
+
+  const typeFilter = (markType) => {
+    markAll.forEach((mark) => {
+      mark.style.display = "none";
+      mark.classList.remove("animated", "fadeIn");
+    });
+
+    no.style.display = "none";
+    no.classList.remove("animated", "fadeIn");
+
+    if (markType) {
+      markType.forEach((mark) => {
+        mark.style.display = "block";
+        mark.classList.add("animated", "fadeIn");
+      });
+    } else {
+      no.style.display = "block";
+      no.classList.add("animated", "fadeIn");
+    }
+  };
+
+  items.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      let selector = e.target.classList[0];
+      if (selector === "grandmother" || selector === "granddad") {
+        typeFilter();
+      } else {
+        const markType = wrapper.querySelectorAll(`.${selector}`);
+        typeFilter(markType);
+      }
+    });
+  });
+
+  menu.addEventListener("click", (e) => {
+    let target = e.target;
+    if (target) {
+      items.forEach((btn) => btn.classList.remove("active"));
+      target.classList.add("active");
+    }
+  });
+};
+
+export default filter;
